fdea10b6e31dd383666408ed27dac10ee119e68e
Artem Denysov <artemd@clioonline.dk>
2015-01-05 20:45:23 +0200

    add mode feature

--- LazyLoading.php
+++ LazyLoading.php
@@ -8,6 +8,7 @@
 namespace denar90\lazyloading;
 
 use yii\base\Module;
+use yii\base\InvalidConfigException;
 
 /**
  * Yii2 module for content lazy loading.
@@ -22,7 +23,78 @@ class LazyLoading extends Module {
     /** @var string $modelNamespace Namespace of your items model*/
     public $modelNamespace = '';
 
+    /** @var string $mode Mode can be:
+     * 'list' - only list of items
+     * 'view' - only list of items with link on item view
+     * 'edit' - only list of items with link on item view and on remove
+     */
+    public $mode = 'list';
+
+    /**
+     * @var array $additionalLinks List of controllers and views.
+     *
+     * For example,
+     *
+     * ```php
+     * [
+     *  'view' => [
+     *      'controller' => 'site',
+     *      'action' => 'itemview'
+     *  ],
+     *  'delete' => [
+     *      'controller' => 'site',
+     *      'action' => 'itemremove'
+     *  ],
+     *
+     * ]
+     * ```
+     */
+    public $additionalLinks = [];
+
     public function init() {
         parent::init();
+        $this->validateParams();
+    }
+
+    /**
+     * Validate params for mode.
+     * @throws \yii\base\InvalidConfigException
+     */
+    private function validateParams() {
+        switch($this->mode) {
+            case 'list' :
+                if (!empty($this->additionalLinks)) {
+                    throw new InvalidConfigException("Property 'additionalLinks' should not be felt");
+                }
+                break;
+            case 'view' :
+                if (empty($this->additionalLinks)) {
+                    throw new InvalidConfigException("Property 'additionalLinks' should be felt");
+                } else {
+                    if (!array_key_exists('view', $this->additionalLinks)) {
+                        throw new InvalidConfigException("Property 'view' with 'controller' and 'action' should be added in 'additionalLinks'");
+                    } else {
+                        if (!isset($this->additionalLinks['view']['controller']) || !isset($this->additionalLinks['view']['action'])) {
+                            throw new InvalidConfigException("Property 'controller' and 'action' should be added");
+                        }
+                    }
+                }
+                break;
+            case 'edit' :
+                if (empty($this->additionalLinks)) {
+                    throw new InvalidConfigException("Property 'additionalLinks' should be felt");
+                } else {
+                    if (!array_key_exists('view', $this->additionalLinks)) {
+                        throw new InvalidConfigException("Property 'view' with 'controller' and 'action' should be added in 'additionalLinks'");
+                    } else if (!isset($this->additionalLinks['view']['controller']) || !isset($this->additionalLinks['view']['action'])) {
+                        throw new InvalidConfigException("Property 'controller' and 'action' should be added");
+                    } else if (!array_key_exists('delete', $this->additionalLinks)) {
+                        throw new InvalidConfigException("Property 'delete' with 'controller' and 'action' should be added in 'additionalLinks'");
+                    } else if (!isset($this->additionalLinks['delete']['controller']) || !isset($this->additionalLinks['delete']['action'])) {
+                        throw new InvalidConfigException("Property 'controller' and 'action' should be added");
+                    }
+                }
+                break;
+        }
     }
 }
